a79cf076afaef2811c9d87f7f00695b81b1cbed3
Kreton16 <renko0708@gmail.com>
2022-12-02 02:19:24 +0200

    f_c

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../pr8.1.it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[101] = {'a', 'b', 'g', 'a', 'a', 'c', 'c', 'c', 'h', 'j', 'u', 'i', 'a', 'a', 'n', 'm', 'k', 'c'};
+			int k = Count(str);
+			Assert::AreEqual(k, 4);
+
+		}
+	};
+}
